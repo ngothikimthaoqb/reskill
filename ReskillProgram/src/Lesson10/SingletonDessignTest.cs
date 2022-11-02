b57f651beda899530573484756e9f38b9bfde9e1
thaongothikim <thao_ngo@epam.com>
2022-11-02 23:37:37 +0700

    Lesson10 - Object Oriented Design Principles

--- ReskillProgram/src/Lesson10/SingletonDessignTest.cs
+++ ReskillProgram/src/Lesson10/SingletonDessignTest.cs
@@ -1,27 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace ReskillProgram.src.Lesson10
 {
     public class SingletonDessignTest
     {
-        /*
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Type brand:");
-            string branch = Console.ReadLine();
-            Console.WriteLine("Type model:");
-            string model = Console.ReadLine();
-            Console.WriteLine("Type quantity:");
-            int quantity = int.Parse(Console.ReadLine());
-            Console.WriteLine("Type cost :");
-            int cost = int.Parse(Console.ReadLine());
-           
+            List<Car> cars = new List<Car>();
 
+            Console.WriteLine("Number of car:");
+            int numberOfCar = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCar; i++)
+            {
+                Console.WriteLine("Type brand:");
+                string branch = Console.ReadLine();
+                Console.WriteLine("Type model:");
+                string model = Console.ReadLine();
+                Console.WriteLine("Type quantity:");
+                int quantity = int.Parse(Console.ReadLine());
+                Console.WriteLine("Type cost :");
+                int cost = int.Parse(Console.ReadLine());
+
+                Car car = new Car(branch, model, quantity, cost);
+                cars.Add(car);
+            }
+
+            CarCollection carCollection = new CarCollection();
+            carCollection.averageCost(cars);
+            carCollection.averagePriceType(cars);
+            carCollection.countTypes(cars);
         }
-        */
+
     }
 }
+
